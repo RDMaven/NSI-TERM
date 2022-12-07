fb4f48e2d528323154f640c44ae0f9b47c9835c1
maven <maxjosephantoine@gmail.com>
2022-12-07 18:28:47 +0100

    smol update for ctf1 prologin

--- PROLOGIN/ctf_1.c
+++ PROLOGIN/ctf_1.c
@@ -3,86 +3,65 @@
 /* WARNING: Instruction at (ram,0x0010132e) overlaps instruction at (ram,0x0010132b)
     */
 
-undefined8 main(void)
+int main(void)
 
 {
   int iVar1;
-  size_t sVar2;
-  char *pcVar3;
-  long lVar4;
+  int extraout_EAX;
+  size_t input_length;
+  char *pcVar2;
+  long i;
   long unaff_RBP;
-  undefined8 *puVar5;
+  undefined8 *puVar3;
   long in_FS_OFFSET;
-  char cVar6;
-  bool bVar7;
-  byte bVar8;
-  undefined8 local_418;
+  char cVar4;
+  bool boolean;
+  int bVar5;
+  char user_input;
   undefined8 local_410;
   undefined8 local_408 [127];
   long local_10;
   
-  bVar8 = 0;
-  local_10 = *(long *)(in_FS_OFFSET + 0x28);
-  getenv("LINES");
-  getenv("COLUMNS");
+  bVar5 = 0;
+  local_10 = *(long *)(in_FS_OFFSET + 40);
+  //getenv("LINES");
+  //getenv("COLUMNS");
   puts("Enter your Serial: ");
-  local_418 = 0;
+  _user_input = 0;
   local_410 = 0;
-  puVar5 = local_408;
-  for (lVar4 = 0x7e; lVar4 != 0; lVar4 = lVar4 + -1) {
-    *puVar5 = 0;
-    puVar5 = puVar5 + (ulong)bVar8 * -2 + 1;
-  }
-  iVar1 = __isoc99_scanf("%1024s",&local_418);
-  bVar8 = (byte)lVar4;
+  puVar3 = local_408;
+  *puVar3 = 0;
+
+  iVar1 = __isoc99_scanf("%1024s",&user_input);
+  bVar5 = 0;
   fflush(stdin);
   if (iVar1 != -1) {
     syscall();
-    sVar2 = strlen((char *)&local_418);
-    iVar1 = FUN_0010142e(&local_418,sVar2);
-    if (iVar1 == 0) {
-      LOSE();
+    input_length = strlen(&user_input);
+    if (extraout_EAX == 0) {
+      loose();
     }
     else {
-      WIN();
+      win();
     }
-    cVar6 = '\0';
-    bRam0000000000000001 = bRam0000000000000001 & bVar8;
-    bVar7 = bRam0000000000000001 == 0;
-    pcVar3 = (char *)(**(code **)(unaff_RBP + -0x3f00b762))();
-    if (!bVar7) {
+  }
+   
+  /* cVar4 = '\0';
+    boolean = False;
+    pcVar2 = (char *)(**(code **)(unaff_RBP + -0x3f00b762))();
+    if (!boolean) {
       if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
-                    /* WARNING: Subroutine does not return */
+                    WARNING: Subroutine does not return
         __stack_chk_fail();
       }
       return 0;
     }
-    *pcVar3 = *pcVar3 + bVar8 + cVar6;
-                    /* WARNING: Bad instruction - Truncating control flow here */
-    halt_baddata();
+    *pcVar2 = *pcVar2 + 0 + '\0';
+                     WARNING: Bad instruction - Truncating control flow here    halt_baddata();
   }
-                    /* WARNING: Subroutine does not return */
+                     WARNING: Subroutine does not return
+              
   exit(1);
-}
-
-
-/* WARNING: Control flow encountered bad instruction data */
-
-void FUN_0010142e(undefined8 param_1,long param_2)
-
-{
-  long in_FS_OFFSET;
-  
-  if (param_2 == 0x19) {
-                    /* WARNING: Call to offcut address within same function */
-    func_0x0010145d();
-                    /* WARNING: Bad instruction - Truncating control flow here */
-    halt_baddata();
-  }
-  if (*(long *)(in_FS_OFFSET + 0x28) == *(long *)(in_FS_OFFSET + 0x28)) {
-    return;
-  }
-                    /* WARNING: Subroutine does not return */
-  __stack_chk_fail();
+  */
 }
 
