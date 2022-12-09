d163d5e4e88f3e0ff1a72fae17bd7d621bff7e2a
RDMaven-EOS <maxjosephantoine@gmail.com>
2022-12-09 08:17:14 +0100

    tours hanoi

--- PILES_LISTES/Tours_Hanoi.py
+++ PILES_LISTES/Tours_Hanoi.py
@@ -1,2 +1,85 @@
+    
+def creer_pile():
+    return []
+
+def est_vide(pile:list):
+    return len(pile)==0
+
+def empiler(pile:list,elt):
+    pile.append(elt)
+
+def empilerPl(pile:list, elt:list):
+    pile.extend(elt)
+
+def depiler(pile:list):
+    assert not est_vide(pile), "La pile est vide"
+    return pile.pop()
+
+def sommet(pile:list):
+    assert not est_vide(pile), "La pile est vide"
+    return pile[-1]
+
+def etatPile(pile:list):
+    return pile
+
+def deplacer(pileInit, pileDest):
+    assert not est_vide(pileInit), "La pile est vide"
+    if est_vide(pileDest) or  sommet(pileInit) < sommet(pileDest):
+        empiler(pileDest, sommet(pileInit))
+        depiler(pileInit)
+
+"""
+p0 = creer_pile()
+p1 = creer_pile()
+p2 = creer_pile()
+
+empilerPl(p0, [4,3,2,1])
+
+deplacer(p0, p2)
+deplacer(p0, p1)
+deplacer(p2, p1)
+"""
 
 
+def etatTours(px:list): # prends p0, p2, p1 comme entrée.
+    return etatPile(px[0]), etatPile(px[2]), etatPile(px[1])
+
+def resoudre(n:int, origine:list, cible:list, interm:list, ordre:list):
+    """Déplacer n disques de l'origine vers la cible.
+
+    Args:
+        n (int): Nombre de disques de la pile de départ à déplacer.
+        origine (list): La pile de départ.
+        cible (list): La pile d'arrivée.
+        interm (list): Une pile intermédiaire.
+    """
+    if n != 0:
+        #deplacer les n-1 disques de l'origine vers l'intermédiaire
+        resoudre(n-1, origine, interm, cible, ordre=[origine,cible,interm])
+        #déplacer le dernier disque de l'origine vers la cible
+        deplacer(origine, cible)
+        print(etatTours(ordre))
+        #déplacer les n-1 disques de l'intermédiaire vers la cible
+        resoudre(n-1, interm, cible, origine, ordre=[origine,cible,interm])
+
+
+def toursHanoi(disques):
+    p0 = creer_pile()
+    p1 = creer_pile()
+    p2 = creer_pile()
+
+    empilerPl(p0, [i for i in range(disques,0,-1)])
+
+    print("\033[32;1m", f"Etat initial du jeu : {etatTours([p0,p1,p2])}")
+    print("\033[37;0m", "")
+
+    resoudre(disques, p0, p2, p1, ordre=[p0,p1,p2])
+
+
+
+if __name__ == "__main__":
+    toursHanoi(4)
+
+    """print("\033[32;1m", "zjrksmdfqj")
+    print("\033[31;1m", "zjrksmdfqj")
+    print("\033[37;0m", "zjrksmdfqj")"""
